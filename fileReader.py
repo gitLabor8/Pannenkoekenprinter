af6a170b26293a535de212e9b61a3910193cf02e
gitLabor8 <frankgerlings37@gmail.com>
2019-02-03 17:09:39 +0100

    Rewrote hardcoded examples to external files

--- fileReader.py
+++ fileReader.py
@@ -8,7 +8,7 @@ from coordinates import Coordinate as Co
 Co.default_order = 'xy'
 
 
-class Examples:
+class FileReader:
     def __init__(self, maxX: float, maxY: float):
         self.maxX = maxX
         self.maxY = maxY
@@ -47,24 +47,18 @@ class Examples:
         mouth = [Co(3, 3), Co(3, 6), Co(5, 6), Co(5, 7), Co(7, 7), Co(7, 6),
                  Co(9, 6), Co(9, 3), Co(8, 3), Co(8, 4), Co(4, 4), Co(4, 3)
                  ]
-        # mouthFill = [Co(5, 5), Co(7, 5)]
-        total = [leftEye, rightEye, mouth]  # , mouthFill]
+        total = [leftEye, rightEye, mouth]
         return self.scale(self.gridify(total, res, res))
 
     # Pokéball from Pokémon
     def pokeball(self):
         res = 10.
-        # upperBall = [Co(1, 4), Co(1, 7), Co(4, 10), Co(7, 10), Co(10, 5)]
-        # lowerBall = [Co(10,4), Co(7,1), Co(4,1), Co(1,4)]
         outerBall = [Co(4, 1), Co(1, 4), Co(1, 7), Co(4, 10),
                      Co(7, 10), Co(10, 7), Co(10, 4), Co(7, 1), Co(4, 1)]
-        # innerBallTop = [Co(1, 5), Co(4, 5), Co(5, 7),
-        #                 Co(6, 7), Co(7, 5), Co(10, 5)]
-        innerBallTop = [Co(10,5), Co(7,5), Co(6,7),
-                        Co(5,7), Co(4,5), Co(1,5)]
+        innerBallTop = [Co(10, 5), Co(7, 5), Co(6, 7),
+                        Co(5, 7), Co(4, 5), Co(1, 5)]
         innerBallBot = [Co(7, 5), Co(6, 4), Co(5, 4), Co(4, 5)]
-        # fillTop = [Co(2, 6), Co(5, 9), Co(6, 9), Co(9, 7)]
-        total = [outerBall, innerBallTop, innerBallBot]  # , fillTop]
+        total = [outerBall, innerBallTop, innerBallBot]
         return self.scale(self.gridify(total, res, res))
 
     # A heartshape,
@@ -76,3 +70,38 @@ class Examples:
                       Co(8, 9), Co(10, 9), Co(10, 6), Co(6, 2)]
         total = [outerHeart, innerHeart]
         return self.scale(self.gridify(total, res, res))
+
+    # Parses one vector following the format: x1, y1 - x2, y2
+    def parseVector(self, line):
+        try:
+            vector = []
+            coordinates = line.strip().split('-')
+            for coordinate in coordinates:
+                x, y = coordinate.strip().split(',')
+                parsedCo = Co(int(x), int(y))
+                vector.append(parsedCo)
+        except ValueError:
+            print("Malformulated vectorline: " + line)
+            print("We will print the figure without this vector")
+        return vector
+
+    # Parses files to printable lists of vectors
+    # Lines that start with '#' are considered comments
+    # The first line gives the x and y resolution
+    # All following lines give one vector each
+    def parse(self, filename):
+        xRes, yRes = 0, 0
+        totalVec = []
+        with open(filename, 'r') as f:
+            for line in f:
+                line = line.strip()
+                # Neglect comments
+                if not line.startswith("#"):
+                    # Catch the resolutions
+                    if xRes == 0 and yRes == 0:
+                        xRes, yRes = line.strip().split(',')
+                        xRes, yRes = int(xRes.strip()), int(xRes.strip())
+                    # Parse a vector
+                    else:
+                        totalVec.append(self.parseVector(line))
+        return self.scale(self.gridify(totalVec, xRes, yRes))
